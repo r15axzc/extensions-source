6059467e092486dc0a3cdf0ab650c5255cde5252
HYDRO-WIN10\Hydro-Win10 <feslta.renerenzxe7@gmail.com>
2025-12-11 20:31:51 +0700

    fix manga168 some pages not show

--- src/th/manga168/src/eu/kanade/tachiyomi/extension/th/manga168/Manga168.kt
+++ src/th/manga168/src/eu/kanade/tachiyomi/extension/th/manga168/Manga168.kt
@@ -1,7 +1,11 @@
 package eu.kanade.tachiyomi.extension.th.manga168
 
+import android.util.Log
 import eu.kanade.tachiyomi.multisrc.mangathemesia.MangaThemesia
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SManga
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonPrimitive
 import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -22,5 +26,38 @@ class Manga168 : MangaThemesia(
                 genre = genre?.plus(", Color")
             }
         }
+    }
+
+    private fun normalizeUrl(url: String): String {
+        // Replace escaped slashes
+        return url.replace("\\/", "/")
+    }
+
+    override fun pageListParse(document: Document): List<Page> {
+        countViews(document)
+
+        val chapterUrl = document.location()
+        val htmlPages = document.select(pageSelector)
+            .filterNot { it.imgAttr().isEmpty() }
+            .mapIndexed { i, img -> Page(i, chapterUrl, img.imgAttr()) }
+        for (page in htmlPages) {
+            Log.w("Manga168", "Page: ${page.imageUrl}")
+            page.imageUrl = page.imageUrl?.let { normalizeUrl(it) }
+        }
+        // Some sites also loads pages via javascript
+        if (htmlPages.isNotEmpty()) { return htmlPages }
+
+        val docString = document.toString()
+        val imageListJson = JSON_IMAGE_LIST_REGEX.find(docString)?.destructured?.toList()?.get(0).orEmpty()
+        val imageList = try {
+            json.parseToJsonElement(imageListJson).jsonArray
+        } catch (_: IllegalArgumentException) {
+            emptyList()
+        }
+        val scriptPages = imageList.mapIndexed { i, jsonEl ->
+            Page(i, chapterUrl, jsonEl.jsonPrimitive.content)
+        }
+
+        return scriptPages
     }
 }
